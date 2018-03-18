00129e976082f02cee196a321abe8f9a768166b5
Pedro <pedro.ivo@hotmail.com.br>
2018-03-18 18:05:46 -0300

    CREATED SQLITE FUN CREATED RESOURCES FOLDER CREATED TEMPLATE.SQL UPDATED GITGNORE UPDATE POM.XML //KANAPKA

--- src/main/java/com/mycompany/app/main/Main.java
+++ src/main/java/com/mycompany/app/main/Main.java
@@ -1,14 +1,14 @@
 package com.mycompany.app.main;
 
-        import com.mycompany.app.banco.Cliente;
-        import com.mycompany.app.banco.Relatorios;
-        import com.mycompany.app.banco.TipoConta;
+import com.mycompany.app.banco.*;
 
 public class Main
 {
     public static void main(String... args)
     {
         Cliente cli = new Cliente("Kiber", "69696969696", TipoConta.Poupanca);
+
+        cli = cli.deposita(300.50).saca(10.0);
 
         Relatorios.relatorio().informacoesCliente(cli);
     }
