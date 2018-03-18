c40b9e115507ee9783e225de5c4aa93c118a9cde
Pedro <pedro.ivo@hotmail.com.br>
2018-03-18 15:11:11 -0300

    TRANSACAO CLASS CREATED TIPOTRANSACAO ENUM
    //KANAPKA

---
+++ src/main/java/com/mycompany/app/banco/Transacao.java
@@ -0,0 +1,58 @@
+package com.mycompany.app.banco;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Transacao
+{
+    public final double valor;
+    public final TipoTransacao opTransacao;
+    public final Date trxDate;
+
+    public Transacao(double valor, TipoTransacao opTransacao, Date trxDate)
+    {
+        this.valor = valor;
+        this.opTransacao = opTransacao;
+        this.trxDate = trxDate;
+    }
+
+    public Transacao withValor(double valor)
+    {
+        return new Transacao(valor, opTransacao, trxDate);
+    }
+
+    public Transacao withTipoTransacao(TipoTransacao opTransacao)
+    {
+        return new Transacao(valor, opTransacao, trxDate);
+    }
+
+    public Transacao withDate(Date trxDate)
+    {
+        return new Transacao(valor, opTransacao, trxDate);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transacao transacao = (Transacao) o;
+        return Double.compare(transacao.valor, valor) == 0 &&
+                opTransacao == transacao.opTransacao &&
+                Objects.equals(trxDate, transacao.trxDate);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(valor, opTransacao, trxDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Transacao{" +
+                "valor=" + valor +
+                ", opTransacao=" + opTransacao +
+                ", trxDate=" + trxDate +
+                '}';
+    }
+}
